04896cec9895f3f9a94a6d051a9d67c7e59c2794
Cleberton-Oliveira <clebertonoliveira.cnt@gmail.com>
2019-09-15 16:06:25 -0300

    Crinado user a partir do e-mail

--- oelixis/oelixis/settings.py
+++ oelixis/oelixis/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'programas'
+    'programas',
+    'cuser'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+AUTH_USER_MODEL = 'cuser.CUser'
